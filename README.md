9b02d218be3fd313196727ba238f85f7e195430e
Nick <dev@nsenger.com>
2020-03-08 20:07:45 -0700

    chore: README

---
+++ README.md
@@ -0,0 +1,9 @@
+# syf
+
+`syf` is a command line utility for downloading Grateful Dead soundboards.
+
+Available commands:
+
+- `syf list`: lists all available soundboards in chronological order
+- `syf fetch {name}`: downloads the specified soundboard to the current working directory
+- `syf fetch all`: downloads all available soundboards to the current working directory
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(arg) = args.get(1) {
+        match &arg[..] {
+            "list" => println!("Fetching list of shows.."),
+            "fetch" => {
+                if let Some(target) = args.get(2) {
+                    println!("Fetching show {}", target)
+                } else {
+                    println!("Please specify a show to fetch or `all`")
+                }
+            }
+            _ => println!(
+                "Invalid command. Valid options are `list`, `fetch $showname`, or `fetch all`"
+            ),
+        }
+    } else {
+        let message = "You need to specify something to do. \
+        Valid options are `list`, `fetch $showname`, or `fetch all`";
+        println!("{}", message);
+    }
 }
